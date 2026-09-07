b343c811ac68b9d5b8d1a10d3cf5f2f123bdd90e
Prashant Yadav <prash08484@gmail.com>
2026-09-07 12:39:00 +0530

    update issue workign

--- backend/src/routes/issue.routes.js
+++ backend/src/routes/issue.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createIssue, getAllIssues, getIssueById, updateIssueById, deleteIssueById } from '../controllers/issue.controller.js';
 import authenticate from '../middlewares/authe.middleware.js';
-import { authorizeIssueAccess, authorizeRepositoryOwner } from '../middlewares/autho.middleware.js';
+import { authorizeIssueAccess, authorizeRepositoryExists } from '../middlewares/autho.middleware.js';
 
 const issueRouter = express.Router();
 
 
-issueRouter.post('/createIssue/:id', authenticate, authorizeRepositoryOwner, createIssue);
+issueRouter.post('/createIssue/:id', authenticate, authorizeRepositoryExists, createIssue);
 issueRouter.get('/allIssues/:id', getAllIssues);
 issueRouter.get('/:issueId', getIssueById);
 issueRouter.put('/:id', authenticate, authorizeIssueAccess, updateIssueById);
